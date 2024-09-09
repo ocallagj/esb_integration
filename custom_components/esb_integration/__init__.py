b7d4b9dc4461fa7e7056eef19006e90d5238e4e1
johnocds <john@dope.security>
2024-09-09 20:32:05 +0100

    Rename to esb_integration

--- custom_components/esb_integration/__init__.py
+++ custom_components/esb_integration/__init__.py
@@ -1,8 +1,8 @@
 """
-Custom integration to integrate integration_blueprint with Home Assistant.
+Custom integration to integrate esb_integration with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/ocallagj/esb_integration
 """
 
 from __future__ import annotations
--- custom_components/esb_integration/const.py
+++ custom_components/esb_integration/const.py
@@ -1,8 +1,8 @@
-"""Constants for integration_blueprint."""
+"""Constants for esb_integration."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
+DOMAIN = "esb_integration"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
